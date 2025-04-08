5c38535ddf2a51b8269a35cfb4a6756aee5fbc56
piguchi@geolonia.com <piguchi@geolonia.com>
2025-04-08 09:24:56 +0900

    環境構築

---
+++ src/components/Map.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import maplibregl from 'maplibre-gl';
+import './Map.css';
+
+
+declare global {
+    interface Window {
+      geolonia: {
+        Popup: new (options?: maplibregl.PopupOptions) => maplibregl.Popup;
+        Map: new (options: maplibregl.MapOptions) => maplibregl.Map;
+        GeolocateControl: new (options: maplibregl.GeolocateControlOptions) => maplibregl.GeolocateControl;
+        NavigationControl: new (options?: maplibregl.NavigationControlOptions) => maplibregl.NavigationControl;
+        Marker: new (options?: maplibregl.MarkerOptions) => maplibregl.Marker;
+      }
+    }
+}
+
+const Map: React.FC = () => {
+    const mapContainer = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!mapContainer.current) return;
+
+        // MapLibreのインスタンスを作成
+        const map = new maplibregl.Map({
+            container: mapContainer.current, // 地図を描画するコンテナ
+            style: "https://geoloniamaps.github.io/basic-v1/style.json", // スタイルURL
+            center: [139.6917, 35.6895], // 初期の中心座標（東京）
+            zoom: 10, // 初期のズームレベル
+        });
+
+        // クリーンアップ処理
+        return () => {
+            map.remove();
+        };
+    }, []);
+
+    return (
+        <div
+            className='map'
+            ref={mapContainer}
+            data-lang="ja"
+            data-gesture-handling="off"
+            data-navigation-control="off"
+            data-scale-control="on"
+        />
+    );
+};
+
+export default Map;
